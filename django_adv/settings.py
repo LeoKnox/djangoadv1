21402211cca573260f296406319047dbf53b71ab
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-12 19:28:24 -0700

    Add files via upload

--- django_adv/settings.py
+++ django_adv/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # mine
+    # make app python manage.py startapp <appname>
+    'pages',
     'rooms',
 ]
 

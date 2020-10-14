6609b8ef4d2a22f29c40877cb1b83ab50a99aade
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-13 17:34:57 -0700

    Add files via upload

--- django_adv/urls.py
+++ django_adv/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, addroom_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
+    path('addroom/', addroom_view, name='addroom'),
     path('admin/', admin.site.urls),
 ]

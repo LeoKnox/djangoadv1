e3d5bf8f5fe19d9904b07b8b9b6a67bf1bb04a87
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-13 18:49:59 -0700

    Add files via upload

--- pages/views.py
+++ pages/views.py
@@ -1,1 +1,13 @@
-a
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request, *args, **kwargs):
+    my_context = {
+        "room_name": "Entry",
+        "width": 5,
+        "length": 5
+    }
+    return render(request, "home.html", my_context)
+
+def addroom_view(request, *args, **kwargs):
+    return render(request, "addroom.html", {})

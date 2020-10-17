7eee939ce2fcf130e8a299d8693db9936452579a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-17 14:55:19 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
+
+def room_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "room/detail.html", {})
 
 def room_detail_view(request):
     #obj = Room.objects.get(id=1)

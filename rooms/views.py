82f5eb82728ec539a725a1460e387cd195af872e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-17 15:22:52 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -3,13 +3,15 @@ from .models import Room
 from .forms import RoomForm
 
 def room_create_view(request):
-    form = ProductForm(request.POST or None)
+    print("form: " )
+    print("context: ")
+    form = RoomForm(request.POST or None)
     if form.is_valid():
         form.save()
     context = {
         'form': form
     }
-    return render(request, "room/detail.html", {})
+    return render(request, "room/addroom.html", context)
 
 def room_detail_view(request):
     #obj = Room.objects.get(id=1)
